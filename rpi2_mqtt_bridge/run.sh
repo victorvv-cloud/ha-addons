fd9ad9921362b0ccc8cb923c4bf640f6b07789f3
victorvv-cloud <victorvv@bk.ru>
2026-07-01 23:28:48 +0500

    Update run.sh

--- rpi2_mqtt_bridge/run.sh
+++ rpi2_mqtt_bridge/run.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bashio
 
-CONFIG_PATH=/data/options.json
 CONFIG_FILE=$(bashio::config 'config_file')
 
-# Запускаем контейнер из официального образа rpi2home-assistant
-docker run \
-  --name rpi2mqtt \
-  --restart unless-stopped \
-  --privileged \
-  --network host \
-  -v /dev:/dev:ro \
-  -v /run/udev:/run/udev:ro \
-  -v "${CONFIG_FILE}:/etc/rpi2home-assistant.yaml:ro" \
-  ghcr.io/f18m/rpi2home-assistant:latest \
-  --config-file /etc/rpi2home-assistant.yaml
+exec python3 -m rpi2home_assistant \
+  --config-file "${CONFIG_FILE}"
